a8c09243d3767fcd8ec8e6aca9d662cf8ffe0d61
shahimmo <mohammadamin.shahimian@drei.com>
2019-12-05 15:01:59 +0100

    test commit from VSC

--- ex0205.py
+++ ex0205.py
@@ -1,4 +1,4 @@
-#find a random nember les than 10 which chosen with the user
+#find a random nember less than 10 which chosen with the user
 import random
 asw=input ('guss a number less than 10!did you?(Y/N)')
 
@@ -10,17 +10,17 @@ if (asw=='Y' or asw=='y'):
         print ('is it ',guess)
         answ3=input('Y/N?')
         if(answ3=='y' or answ3=='Y'):
-            print('you win!')
+            print('yuhooooo!you win!')
         elif(answ3=='N' or 'n'):
-            print('try again! line 12')
+            print('fuck.try again! )
 
     elif(answ2=='n' or answ2=='N'):
         guess2=random.randint(6,10)
         print('is it ',guess2)
         answ4=input('Y/N?')
         if(answ4=='y' or answ4=='Y'):
-            print('you win!')
+            print('Oh yeee.you win!')
         elif(answ4=='N' or answ4=='n'):
-            print('try again! line 22')
+            print('Shit.try again!')
 print ('See you!')
 
