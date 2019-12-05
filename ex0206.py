a2d30bb22650061c6c5393f2bb67fd583a0effff
shahimmo <mohammadamin.shahimian@drei.com>
2019-12-05 16:03:17 +0100

    bar guard, play with try except

--- ex0206.py
+++ ex0206.py
@@ -1,1 +1,16 @@
 # play with try/except
+#input a value and answer if user is adult or not 
+ageString=input('how olds are you?')
+
+try:
+    age=int(ageString)
+except :
+    age=-1
+
+if age>18:
+    print('you are an adult! you can enjoy the bar!') 
+elif age>0:
+    print('you are younger than 18! you can not get in the bar!') 
+else:
+    print('try again! invalid input!')
+print('finish')
